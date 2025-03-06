23d9c0b3ae3c0380a6143319a3a98a65935f9853
Pradeva <90541409+Pradeva@users.noreply.github.com>
2025-03-06 10:43:22 +0700

    Login, Login admin, Register

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,22 @@ import './global.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { AppProvider } from './context/AppContext.jsx'
+import Login from './pages/login.jsx'
+import LoginAdmin from './pages/LoginAdmin.jsx'
+import Register from './pages/Register.jsx'
+import { Toaster } from 'react-hot-toast'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AppProvider>
+    <Toaster />
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/login-admin' element={<LoginAdmin />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
     </BrowserRouter>
     </AppProvider>
